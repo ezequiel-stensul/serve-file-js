95e88f374ce998fdeac205edcd230d3a4e61493f
ezequiel-stensul <126105777+ezequiel-stensul@users.noreply.github.com>
2023-02-27 15:11:02 -0300

    Update some-malicious-js-file.js

--- some-malicious-js-file.js
+++ some-malicious-js-file.js
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+let add_element = () => {
+    const template = document.createElement('div');
+    template.innerHTML = "Content inside DIV";
+    
+    document.body.appendChild(template);
+}
+
+add_element();
